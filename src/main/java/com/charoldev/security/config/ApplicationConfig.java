5242d20e30cc3e649ac6111bbbf02e660c2cf22e
Harold Rojas <harolrop14@gmail.com>
2023-08-19 03:26:11 -0500

    ready to create paths

--- src/main/java/com/charoldev/security/config/ApplicationConfig.java
+++ src/main/java/com/charoldev/security/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.charoldev.security.config;
 
 import com.charoldev.security.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
